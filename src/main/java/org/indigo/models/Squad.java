e8826015934ec8e0f71dd9eb3a08b9822bf7e39b
Violet <vnjuguna@gmail.com>
2023-07-13 18:39:59 +0300

    added the data types on hero and squad class

--- src/main/java/org/indigo/models/Squad.java
+++ src/main/java/org/indigo/models/Squad.java
@@ -2,4 +2,43 @@ package org.indigo.models;
 
 class Squad {
 
+    private int id;
+
+    private String name;
+
+    private int maxSize;
+
+    private String cause;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
 }
