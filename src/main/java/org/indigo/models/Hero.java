a89b876e022ffe4f4f1a435048663f605159d9c5
Violet <vnjuguna@gmail.com>
2023-07-13 18:46:27 +0300

    made edits to the hero class

--- src/main/java/org/indigo/models/Hero.java
+++ src/main/java/org/indigo/models/Hero.java
@@ -1,5 +1,60 @@
 package org.indigo.models;
 
-class Hero {
+import java.util.Date;
 
+class Hero {
+    private int id;
+    private String name;
+    private Date dateOfBirth;
+    private String power;
+    private String weakness;
+    private String squad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public String getSquad() {
+        return squad;
+    }
+
+    public void setSquad(String squad) {
+        this.squad = squad;
+    }
 }
