1840c3fc5aa9dd7b174f77f87bd2cd4082943a70
Violet <vnjuguna@gmail.com>
2023-07-26 00:56:32 +0300

    generate the getters and setters

--- src/main/java/ke/co/safaricom/models/Strength.java
+++ src/main/java/ke/co/safaricom/models/Strength.java
@@ -1,4 +1,44 @@
 package ke.co.safaricom.models;
 
 public class Strength {
+    private int id;
+    private String name;
+    private int score;
+    private boolean deleted = false;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Strength() {
+
+    }
 }
