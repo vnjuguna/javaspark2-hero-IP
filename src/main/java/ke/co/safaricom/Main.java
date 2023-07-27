334527a24051624cf9e3e2b1b123052301e6bc6a
Violet <vnjuguna@gmail.com>
2023-07-28 00:19:09 +0300

    create hero.hbs and the correspondin form to allow entry but with an error connecting to the DB

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,7 +1,9 @@
 package ke.co.safaricom;
 
 
+import ke.co.safaricom.dao.HeroDao;
 import ke.co.safaricom.dao.StrengthDao;
+import ke.co.safaricom.models.Hero;
 import ke.co.safaricom.models.Strength;
 import ke.co.safaricom.utils.SharedUtils;
 import spark.ModelAndView;
@@ -32,5 +34,31 @@ public class Main {
             response.redirect("/");
             return null;
         });
+
+        get("/heroes/add", (request, response) -> {
+            return SharedUtils.render(new HashMap<>(), "create-heroes.hbs");
+        });
+
+        post("/create-heroes", (request, response) -> {
+            String name = request.queryParams("name");
+            int age = Integer.parseInt(request.queryParams("age"));
+            int squad_id = Integer.parseInt(request.queryParams("squad_id"));
+            int strength_id = Integer.parseInt(request.queryParams("strength_id"));
+            int weakness_id = Integer.parseInt(request.queryParams("weakness_id"));
+
+            Hero hero = new Hero();
+            hero.setName(name);
+            hero.setAge(age);
+            hero.setSquad_id(squad_id);
+            hero.setStrength_id(strength_id);
+            hero.setWeakness_id(weakness_id);
+
+            // Assuming there is a valid HeroDao class with a create() method to save the hero object
+            HeroDao.create(hero);
+
+            response.redirect("/");
+            return null;
+        });
+
     }
 }
